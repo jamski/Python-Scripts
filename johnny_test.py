f9ea0a0355ef3c63b15d5cbd98bbc7515d0e8075
Jamski <steve.barnabas@gmail.com>
2013-09-30 17:04:38 -0700

    Python Scripts

---
+++ johnny_test.py
@@ -0,0 +1,8 @@
+# The Johnny Test
+
+name = raw_input("What is your name?")
+ass = raw_input("Do you want Ass or Titties?")
+
+print "WRONG ANSWER! Dont be a Johnny, be a %s. You should not only be thinking about %s. " \
+"You should be thinking about both Ass and Titties." % (name, ass)
+
---
+++ what_johnny_needs.py
@@ -0,0 +1,23 @@
+# WHAT JOHNNY K NEEDS
+
+word1 = "VEGAS"[3]
+word2 = "STEVE"[0]
+word3 = "CHRIS"[4]
+word4 = "JOHNNY K"[6]
+word5 = "&"[0]
+word6 = "NO RULES"[2]
+word7 = "DETERMINATION"[9]
+word8 = "INDICA"[3]
+word9 = "THC"[0]
+word10 = "SHAWIRMA"[4]
+word11 = "BEN"[1]
+word12 = "KABOBS"[5]
+
+the_secret = word1 + word2 +word3 + word4 + word5 + word6 + word7 + word8 + word9 + word10 + word11 + word12
+
+print the_secret
+
+
+
+
+
